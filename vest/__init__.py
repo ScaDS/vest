6830843fc2a806e3ee11e686db59ce68390569d6
Robert Haase <haesleinhuepf@users.noreply.github.com>
2026-04-19 11:20:33 +0200

    bump version

--- vest/__init__.py
+++ vest/__init__.py
@@ -7,7 +7,7 @@ This package provides tools to visualize images placed in 3D space, allowing
 interactive exploration using mouse and keyboard controls.
 """
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 from .app import create_app
 from .data_loader import DataLoader
